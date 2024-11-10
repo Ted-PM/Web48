7ea66e517ef9e6f95169b4a3faa941a577615545
WUStudent <60398328+BigOnake@users.noreply.github.com>
2024-11-10 06:07:35 -0600

    MOVEMENT!!!!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,37 +7,41 @@ public class script_movement : MonoBehaviour
     public Rigidbody playerRB;
     BoxCollider bodyCollider;
     Vector3 _input;
-    private CharacterController _characterController;
-
-    //public float friction; //Speed is multiplied by this value every frame after you stop holding a direction, if you are grounded.                               Recommended value: 0.5
-    //public float speedCap; //Highest possible run speed.                                                                                                          Recommended value: 20
+    private Vector3 lastPos;                                                                                                      
     public float speed = 12;
     public float turnSpeed = 180f;
-    //public float airFriction; //Speed is multiplied by this value every frame after you stop holding a direction, if you are aerial.                              Recommended value: 
-    //public float airResistanceLimit; //Air friction wil
-    //[SerializeField] private bool _grounded;
-    //[SerializeField] private float _horizontalInput;
-    //[SerializeField] private float _verticalInput;
+    private Animator lAnimator;
     void Start()
     {
         bodyCollider = GetComponent<BoxCollider>();
         playerRB = GetComponent<Rigidbody>();
-        _characterController = GetComponent<CharacterController>();
+        lAnimator = GetComponent<Animator>();
+        if (lAnimator != null )
+        {
+            Debug.Log("animator is attached");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GameManager.instance.menu.activeSelf && !ZoomOnObject.instance.start)
-        {
-            setInput();
-            direction();
-        }
+        //if (!GameManager.instance.menu.activeSelf && !ZoomOnObject.instance.start)
+        //{
+        setInput();
+        direction();
+        
+        
+        //}
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         movePlayer();
+
+        
+       lAnimator.SetBool("Walking", _input.magnitude != 0);
+       Debug.Log("Bool is " + lAnimator.GetBool("Walking"));
+        
     }
 
     void setInput()
@@ -52,11 +56,10 @@ public class script_movement : MonoBehaviour
             var rotation = Quaternion.LookRotation(_input, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, turnSpeed);
         }
-
     }
 
     void movePlayer()
     {
-        playerRB.MovePosition((transform.position + (transform.forward * _input.normalized.magnitude) * speed * Time.deltaTime));
+        playerRB.MovePosition((transform.position - (transform.forward * _input.normalized.magnitude) * speed * Time.deltaTime));
     }
 }
